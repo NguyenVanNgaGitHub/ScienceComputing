38f1452f34b817a33fa8c0d3e76a88374ad40ebb
quanganh991 <dinhqanh2012@gmail.com>
2021-05-14 19:54:12 +0700

    Create 2 Stick Distributions

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from ConstantStickDistribution import ConstantStickDistribution
+from LocalCurvatureAtThePointOfContactStickDistribution import LocalCurvatureAtThePointOfContactStickDistribution
+from NumberOfNeighboringParticlesStickDistribution import NumberOfNeighboringParticlesStickDistribution
+
 from DLA import DLA
 
-stickDist = ConstantStickDistribution(proba=0.6)
+# stickDist = ConstantStickDistribution(proba=0.6)
+
+stickDist = LocalCurvatureAtThePointOfContactStickDistribution(A = 1, B = 0.7, percent_box= 80)
+
+# stickDist = NumberOfNeighboringParticlesStickDistribution(A = 0.6, B = 0.75, percent_box=75)
+
 dla = DLA(radius_limit=200,numParticles=5000,stickDistribution=stickDist)
 
 dla.simulate()
